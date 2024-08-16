f77882aabc2981dab2312a80964e455ec7e428b5
Jacob Van Schenck <jacob.vanschenck@gmail.com>
2024-08-16 10:43:53 -0500

    Tests, help, exit

--- repl.go
+++ repl.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func startRepl() {
@@ -12,7 +13,52 @@ func startRepl() {
 	for {
 		fmt.Print(" >")
 		scanner.Scan()
+
 		text := scanner.Text()
-		fmt.Println("echoing: ", text)
+		cleaned := cleanInput(text)
+
+		if len(cleaned) == 0 {
+			continue
+		}
+		commandName := cleaned[0]
+
+		availableCommands := getCommands()
+
+		command, ok := availableCommands[commandName]
+
+		if !ok {
+			fmt.Println("Invalid command")
+			continue
+		}
+
+		command.callback()
+
 	}
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func()
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Prints the help menu",
+			callback:    callbackHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exits the pokedex",
+			callback:    callbackExit,
+		},
+	}
+}
+
+func cleanInput(str string) []string {
+	lowered := strings.ToLower(str)
+	words := strings.Fields(lowered)
+	return words
 }
